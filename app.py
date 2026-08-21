8038d9167b24f3f257b92900ed849ae437bd4d61
Sarvarbek <sarvarravshanbekov16@gmail.com>
2026-08-21 18:24:57 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -989,6 +989,8 @@ def dashboard_screen():
         pass
 
     # --- 2. Dynamic Gamification Gamestate Logic ---
+    streak = 7  # 🛑 TEMPORARY TEST: Change this number to test UI, then delete this line later!
+    
     if streak == 0:
         flame_color = "#64748B" # Slate Gray
         flame_glow = "none"
